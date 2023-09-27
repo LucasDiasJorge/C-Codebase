bdc20c2e71a0c24e4ff22c103b56cb701f0eef9f
Lucas Dias Jorge <78762601+LucasDiasJorge@users.noreply.github.com>
2023-09-27 12:12:05 -0300

    Create main.c

---
+++ Preprocessors/compiler example/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 1000
+
+int main (void) {
+   
+   int v[MAX]; // int v[1000];
+   
+   for (int i = 0; i < MAX; ++i) { // for (int i = 0; i < 1000; ++i)
+      printf("%i\n",i);
+   }
+
+   return EXIT_SUCCESS;
+} 
