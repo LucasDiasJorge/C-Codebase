92e7df8fe27c5f9ebbc04295129052b8d8dfe3f0
Lucas Dias Jorge <78762601+LucasDiasJorge@users.noreply.github.com>
2024-03-18 16:10:51 -0300

    Create main.c

---
+++ Json/Simple/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    FILE *fp;
+    char buffer[1024];
+    int status = 0;
+
+    fp = fopen("example.json", "r");
+
+    if (fp == NULL) {
+        printf("Erro ao abrir o arquivo");
+        return -1;
+    }
+
+    while (fgets(buffer, sizeof(buffer), fp)) {
+        // Check if the line contains the "status" key
+        if (strstr(buffer, "\"status\":") != NULL) {
+            // Extract the value associated with the "status" key
+            if (sscanf(buffer, "%*[^:]:%d", &status) == 1) {
+                break;
+            }
+        }
+    }
+
+    fclose(fp);
+
+    printf("status = %d\n", status);
+
+    return 0;
+}
