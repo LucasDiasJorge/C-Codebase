7084459e53a264c2f69088bd4d152b478d46a360
Lucas Dias Jorge <78762601+LucasDiasJorge@users.noreply.github.com>
2024-09-13 14:08:26 -0300

    Refactoring send file by socket

--- Network/send-file/Sockets/minimal_socket.c
+++ Network/send-file/Sockets/minimal_socket.c
@@ -7,6 +7,7 @@
 #include <stdio.h>
 
 void main() {
+    
     int s = socket(AF_INET, SOCK_STREAM, 0);
     struct sockaddr_in addr = {
         AF_INET,
@@ -16,38 +17,20 @@ void main() {
 
     bind(s, &addr, sizeof(addr));
 
-    printf("Debug\n");
-
     listen(s, 10); // Listen max of 10 connections
 
-    printf("Debug\n");
-
-
     int client_fd = accept(s, 0, 0);
-
-    printf("Debug\n");
-
 
     char buffer[256] = {0};
     recv(client_fd, buffer, 256, 0);
 
     // GET /file.html ..... filter
-
-    printf("Debug\n");
-
-
     char* f = buffer + 5;
     *strchr(f, ' ') = 0;
     int opened_fd = open(f, O_RDONLY);
     
     // File sender
-
-    printf("Debug\n");
-
-
     sendfile(client_fd, opened_fd, 0, 256);
-
-    printf("Debug\n");
 
     close(opened_fd);
     close(client_fd);
